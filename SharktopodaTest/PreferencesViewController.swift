5a08de5bc2a4569649edf06ac937b827161af3a7
jaywardell <jaywardell@mac.com>
2016-08-24 00:11:16 -0400

    Preferences Window is working

---
+++ SharktopodaTest/PreferencesViewController.swift
@@ -0,0 +1,81 @@
+//
+//  PreferencesViewController.swift
+//  UDPServerTest
+//
+//  Created by Joseph Wardell on 8/22/16.
+//  Copyright © 2016 Joseph Wardell. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesViewController: MessageHandlerViewController {
+
+    @IBOutlet weak var portField: NSTextField!
+    @IBOutlet weak var startStopButton: NSButton!
+
+    private var preferredServerPort : UInt16 {
+        
+        return UInt16(portField.intValue)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        startStopButton.target = self
+        startStopButton.action = #selector(startStopButtonPressed(_:))
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        updateUI()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(messageHandlerDidStart(_:)),
+                                                         name: MessageHandler.Notifications.DidStartListening,
+                                                         object: messageHandler)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(messageHandlerDidStop(_:)),
+                                                         name: MessageHandler.Notifications.DidStopListening,
+                                                         object: messageHandler)
+    }
+    
+    override func viewWillDisappear() {
+        super.viewWillDisappear()
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+
+    // MARK:- Updating UI
+    
+    func updateUI() {
+
+        guard let server = messageHandler?.server else { return }
+
+        let port = server.running ? (server.port ?? 0) : NSUserDefaults.standardUserDefaults().preferredServerPort
+
+        portField.stringValue = "\(port)"
+        portField.enabled = !server.running
+        portField.becomeFirstResponder()
+
+        startStopButton.intValue = server.running ? 1 : 0
+    }
+
+    // MARK:- Actions
+
+    func startStopButtonPressed(sender:NSButton) {
+        
+        NSUserDefaults.standardUserDefaults().preferredServerPort = preferredServerPort
+        
+        messageHandler?.toggleServerOnPort(preferredServerPort)
+        
+        NSUserDefaults.standardUserDefaults().startServerOnStartup = sender.integerValue != 0
+    }
+    
+    // MARK:- Notifications
+
+    func messageHandlerDidStart(notification:NSNotification) {
+        updateUI()
+    }
+    func messageHandlerDidStop(notification:NSNotification) {
+        updateUI()
+    }
+
+}
