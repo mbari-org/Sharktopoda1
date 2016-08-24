51b4b375d508609ec3d265516b45b6809c8942fe
jaywardell <jaywardell@mac.com>
2016-08-24 00:16:05 -0400

    added Logging Window.  Server-side is dones

---
+++ SharktopodaTest/LogViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LogViewController.swift
+//  UDPServerTest
+//
+//  Created by Joseph Wardell on 8/22/16.
+//  Copyright © 2016 Joseph Wardell. All rights reserved.
+//
+
+import Cocoa
+
+class LogViewController: MessageHandlerViewController {
+
+    @IBOutlet var logView: NSTextView! {
+        didSet {
+            logView.editable = false
+        }
+    }
+
+    override var messageHandler : MessageHandler? {
+        willSet {
+            messageHandler?.log.removeListener(self)
+        }
+        didSet {
+            if let messageHandler = messageHandler {
+                logView.showLog(messageHandler.log)
+                messageHandler.log.addListener(self)
+            }
+        }
+    }    
+}
+
+extension LogViewController : LogListener {
+    
+    func logChanged(notification: NSNotification) {
+        guard let log = logFromNotification(notification) else { return }
+        guard let messageHandler = messageHandler else { return }
+        guard log === messageHandler.log else { return }
+        
+        logView.showLog(log.log)
+    }
+}
