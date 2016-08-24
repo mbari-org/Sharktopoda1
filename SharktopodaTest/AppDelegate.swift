99fea5976eb5517778c933a41c9c45fdf8eceb1a
jaywardell <jaywardell@mac.com>
2016-08-23 22:04:41 -0400

    Initial Commit

---
+++ SharktopodaTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SharktopodaTest
+//
+//  Created by Joseph Wardell on 8/23/16.
+//  Copyright © 2016 Joseph Wardell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SharktopodaTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SharktopodaTest
+//
+//  Created by Joseph Wardell on 8/23/16.
+//  Copyright © 2016 Joseph Wardell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
